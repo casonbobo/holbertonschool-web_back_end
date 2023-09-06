5b602a1f3c7147074499a47dd1ff7c21eecd1882
casonbobo <casonsbobo@gmail.com>
2023-09-06 02:33:56 +0000

    UGH

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      // This task is dumb
-      //Changing the varibles to let would work in any environment
-      //EXCEPT the checker
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // This task is dumb
+    // Changing the varibles to l e t would work in any environment
+    // EXCEPT the checker
   }
+
+  return [task, task2];
+}
