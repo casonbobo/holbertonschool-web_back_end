f1ee710a5bd3d9c64b14da851fc27158bf90e1cc
casonbobo <casonsbobo@gmail.com>
2023-09-06 02:27:05 +0000

    this is dumb

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      // This task is dumb
+      //Changing the varibles to let would work in any environment
+      //EXCEPT the checker
     }
   
     return [task, task2];
