87215199ac6c73c34d20906489fa8f578d9a074b
casonbobo <casonsbobo@gmail.com>
2023-09-07 21:04:52 +0000

    ughughugh

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name needs to be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length needs to be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!newStudents.every((students) => typeof students === 'string') || !Array.isArray(newStudents)) {
+      throw TypeError('newStudents needs to be a string');
+    }
+    this._students = newStudents;
+  }
+}
+export default HolbertonCourse;
