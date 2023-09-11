0aefeadb3a332460c9340672a4572dc3e101513e
casonbobo <casonsbobo@gmail.com>
2023-09-11 20:45:57 +0000

    maybe this

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
 export default function getListStudentsIds(list) {
-  if (!(Array.isArray(list))) return [];
+  if (!Array.isArray(list)) {
+    return [];
+  }
   return list.map((student) => student.id);
 }
---
+++ ES6_data_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a readme for your late night reading. Don't read too deep into it tho, markdown doesn't have an option for metaphors.
