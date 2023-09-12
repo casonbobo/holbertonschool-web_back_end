796b0b104b6e5afbf4985c2990478cbccb2892d0
casonbobo <casonsbobo@gmail.com>
2023-09-12 18:06:41 +0000

    nooooo

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
   return list.filter((students) => students.location === city)
     .map((student) => {
-      const gradeObject = newGrades.find((item) => item.studentId = student.id);
+      const gradeObject = newGrades.find((item) => item.studentId === student.id);
       const grade = gradeObject ? gradeObject.grade : 'N/A';
-      return {...student, grade};
+      return { ...student, grade };
     });
 }
